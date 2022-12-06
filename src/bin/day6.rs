f605bcfdd013293fff0b467d9ded27bbefb727a9
jafar <aljuneidi.jafar@gmail.com>
2022-12-06 15:54:31 +0200

    day 6, less efficient

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,42 +1,24 @@
 use anyhow::Result;
-use std::collections::HashMap;
 
-fn sliding_window(input: &Vec<char>, win_size: usize) -> Result<usize, ()> {
-    let mut map = HashMap::new();
-
-    for i in 0..=input.len() - win_size {
-        if i == 0 {
-            for j in 0..win_size {
-                map.entry(input[j])
-                    .and_modify(|freq| *freq += 1)
-                    .or_insert(1);
-            }
-        } else {
-            if map.len() == win_size {
-                return Ok(i + win_size - 1);
-            }
-
-            if map[&input[i - 1]] == 1 {
-                map.remove(&input[i - 1]);
-            } else {
-                map.entry(input[i - 1]).and_modify(|freq| *freq -= 1);
-            }
-            map.entry(input[i + win_size - 1])
-                .and_modify(|freq| *freq += 1)
-                .or_insert(1);
-        }
-    }
-    Err(())
+fn sliding_window(input: &str, win_size: usize) -> usize {
+    input
+        .as_bytes()
+        .windows(win_size)
+        .position(|window| {
+            !window
+                .iter()
+                .enumerate()
+                .any(|(index, num)| window[..index].contains(num))
+        })
+        .unwrap()
+        + win_size
 }
 
 fn main() -> Result<()> {
-    let input = include_str!("./day6.txt")
-        .trim()
-        .chars()
-        .collect::<Vec<char>>();
+    let input = include_str!("./day6.txt");
 
-    println!("part1 is :{:?}", sliding_window(&input, 4));
-    println!("part2 is :{:?}", sliding_window(&input, 14));
+    println!("part1 is :{:?}", sliding_window(input, 4));
+    println!("part2 is :{:?}", sliding_window(input, 14));
 
     Ok(())
 }
